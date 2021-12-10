29ab84cbd59fe91de96aa70859c80fa1d423d2d5
rikyyudistira <ryrdn25@gmail.com>
2021-12-10 19:21:59 +0700

    revisi 2

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+  var text = [
+    "Percaya gak sih, simpan aset digital bisa bayarin Saldo OVO kamu seumur hidup?", 
+    "Percaya gak sih, simpan aset digital bisa bayarin Makan kamu seumur hidup?",
+    "Percaya gak sih, simpan aset digital bisa bayarin Voucher Game kamu seumur hidup?",
+    "Percaya gak sih, simpan aset digital bisa bayarin belanja eCommerce kamu seumur hidup?",
+    "Percaya gak sih, simpan aset digital bisa bayarin Kuota Data kamu seumur hidup?"
+  ];
+  var counter = 0;
+  var elem = document.getElementById("changeText");
+
+  function change() {
+    $(elem).fadeTo(3000, 0, function() {
+      this.innerHTML = text[counter];
+      counter = ++counter % text.length;
+      $(this).fadeTo(3000, 8, change)
+    })
+  }
+
+  change();
+});
