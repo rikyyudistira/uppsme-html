c9df2060ab1f556060424ccca791acbd9d74928e
rikyyudistira <ryrdn25@gmail.com>
2021-12-19 10:15:04 +0700

    index2.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,20 @@ $( document ).ready(function() {
       $(this).fadeTo(3000, 8, change)
     })
   }
-
   change();
+
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    center: true,
+    responsive:{
+      0:{
+          items: 1
+      },
+      768:{
+          items: 2.25
+      }
+    }
+  })
 });
